15b6bd06471e91882a2d045d63069426ed43d8ab
Adam Clouse <amclouse96@gmail.com>
2020-03-12 11:07:42 -0400

    update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASe_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     
     dialect: 'postgres',
 });
